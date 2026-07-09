17acf18158ffee068739664536596ea74682e9ab
qkhauh. <quockhanh6244@gmail.com.>
2026-07-09 11:12:09 +0700

    update video 22

--- src/video21/video21.js
+++ src/video21/video21.js
@@ -1,3 +1,6 @@
 console.log('Hello World');
+console.error("oops lỗi cái gì r đó ní ơi!");
+console.warn("cẩn thận ní ơi");
 const name = "halo";
 const age = 25;
+
---
+++ src/video22/video22.js
@@ -0,0 +1,6 @@
+const name = "Thu";
+const age = 22;
+
+const introduction = `Xin chào, tôi tên là ${name} và tôi ${age} tuổi.`;
+
+console.log(introduction);
