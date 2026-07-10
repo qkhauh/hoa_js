63e0b1cf99c272eb078e41551d4ed91ba47eaa55
qkhauh. <quockhanh6244@gmail.com.>
2026-07-10 23:00:47 +0700

    ud video 26

---
+++ Chapter04/video26/video26.js
@@ -0,0 +1,16 @@
+//if // if - else // else if
+const age = 25;
+if (age > 18) {
+    console.log("oke m du 18 tuoi");
+} else {
+    console.log("m chua du 18 tuoi");
+}
+//else if
+const age1 = 25;
+if (age1 > 18) {
+    console.log("oke m du 18 tuoi");
+} else if (age1 == 18) {
+    console.log("m vua du 18 tuoi");
+} else {
+    console.log("m chua du 18 tuoi");
+}
