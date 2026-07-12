ea14b69437847de261b4540c8d1fd2d02278f7ca
qkhauh. <quockhanh6244@gmail.com.>
2026-07-12 22:03:37 +0700

    udvd33

---
+++ Chapter04/video33/video33.js
@@ -0,0 +1,14 @@
+console.log("video33");
+
+let glo = "tôi là biến toàn cục";
+function myFunc() {
+    console.log(glo);
+}
+myFunc();
+console.log(glo);
+
+function sayhi() {
+    let name = "Thu"
+    console.log("xin chào", name)
+}
+sayhi();
