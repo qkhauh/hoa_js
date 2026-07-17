d1a28e4c3d6bcae99dbf2bcded3e46af3171d721
qkhauh. <quockhanh6244@gmail.com.>
2026-07-17 23:00:45 +0700

    hahahaa

---
+++ chapter06/video49/video49.js
@@ -0,0 +1,7 @@
+const elemet = document.getElementById("user");
+
+const myDiv = document.querySelector(".hoidanit");
+
+const allMyDiv = document.querySelectorAll(".hoidanit");// goi het tat ca phan tu html co class hoidanit
+
+console.log(elemet);
---
+++ chapter06/video50/video50.js
@@ -0,0 +1,7 @@
+const elemet = document.getElementById("btn");
+
+console.log(elemet);
+
+function clk() {
+    console.log(`m bam nut lam gi?`);
+}
