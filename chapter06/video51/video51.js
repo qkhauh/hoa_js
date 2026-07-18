0e4fff2eac503530679019f39e54ebeedd5e8a20
qkhauh. <quockhanh6244@gmail.com.>
2026-07-18 23:21:17 +0700

    lambientrongngayhomnay

---
+++ chapter06/video51/video51.js
@@ -0,0 +1,12 @@
+console.log("video51");
+
+const el = document.getElementById("btn");
+
+const tenham = () => {
+    console.log("bam lam cc gi");
+}
+
+el.addEventListener("click", tenham);
+
+
+console.log(el);
---
+++ chapter06/video52/video52.js
@@ -0,0 +1,10 @@
+console.log("video52");
+
+const nut = document.getElementById("nutbam");
+
+const ten = document.getElementById("tuade");
+
+nut.addEventListener("click", () => {
+    console.log("bam lam cc gi")
+})
+
