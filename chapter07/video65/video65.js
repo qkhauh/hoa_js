f9b6b8b579035182ad46bb1bdaf161c068c309ab
qkhauh. <quockhanh6244@gmail.com.>
2026-07-31 23:48:36 +0700

    dicmchungmay

---
+++ chapter07/video65/video65.js
@@ -0,0 +1,12 @@
+console.log('video65');
+
+const ham = (name, vail) => {
+    console.log(`xin chao ${name}`);
+    vail();
+}
+
+const hello = () => {
+    console.log(`adu`);
+}
+
+ham('thuyen', hello);
