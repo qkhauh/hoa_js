9866204c5b2918fe89e3012ae178a2edee907acc
qkhauh. <quockhanh6244@gmail.com.>
2026-07-31 23:58:13 +0700

    djtcme

--- chapter07/video66/video66.js
+++ chapter07/video66/video66.js
@@ -1,1 +1,17 @@
 console.log(`video66`);
+
+//promise
+
+fetch(`https://localhost:8000/users`)
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+
+//async await
+
+const fetchApi = async () => {
+    const res = await fetch(`https://localhost:8000/users`);
+    const data = await res.json();
+    console.log(data);
+}
+fetchApi();
