1c43882512a80efe0f295b141426edf0d924e212
qkhauh. <quockhanh6244@gmail.com.>
2026-08-03 01:08:06 +0700

    hieu

--- chapter08/video70/video70.js
+++ chapter08/video70/video70.js
@@ -28,3 +28,52 @@ if (savebtn) {
         window.location.href = "video70.html";
     })
 }
+
+const generatetodotable = () => {
+    const todoliststr = localStorage.getItem("todo");
+    if (todoliststr) {
+        const todolist = JSON.parse(todoliststr);
+        console.log(todolist);
+
+
+        const tbody = document.querySelector("#todolist tbody");
+
+        if (todolist && todolist.length) {
+            todolist.forEach((todo, index) => {
+                tbody.innerHTML += `
+                   <tr>
+                    <td>${todo.id}</td>
+                    <td>${todo.name}</td>
+                    <td><button data-id="${todo.id}" class="btn-delete">Xoa</button></td>
+                   </tr>
+                `;
+            });
+        }
+    }
+}
+
+
+generatetodotable();
+
+const deletebtn = document.querySelectorAll(".btn-delete");
+if (deletebtn) {
+    deletebtn.forEach((btn, index) => {
+        console.log(btn, index)
+        btn.addEventListener("click", () => {
+            const id = btn.getAttribute("data-id");
+            handdelete(id);
+        })
+    })
+}
+
+const handdelete = (id) => {
+    const currtodostr = localStorage.getItem("todo");
+    if (currtodostr) {
+        const currtodo = JSON.parse(currtodostr);
+        const newtodo = currtodo.filter((todo, index) => todo.id + "" !== id);
+
+        localStorage.setItem("todo", JSON.stringify(newtodo));
+
+        window.location.href = "video70.html";
+    }
+}
