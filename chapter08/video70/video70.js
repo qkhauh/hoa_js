5b1fc8ae971b70f34634fc732c067d2d09d20018
qkhauh. <quockhanh6244@gmail.com.>
2026-08-02 15:36:13 +0700

    kho hieu

--- chapter08/video70/video70.js
+++ chapter08/video70/video70.js
@@ -1,1 +1,21 @@
 console.log(`video 70`);
+
+const savebtn = document.getElementById("btnsave");
+const nametodo = document.getElementById("name");
+
+savebtn.addEventListener('click', () => {
+    const mytodo = {
+        id: 1, name: nametodo.value
+    }
+    const currtodostr = localStorage.getItem("todo");
+
+    if (currtodostr) {
+        const currtodo = JSON.parse(currtodostr);
+
+        currtodo.push(mytodo)
+        localStorage.setItem("todo", JSON.stringify([currtodo]));
+
+    } else {
+        localStorage.setItem("todo", JSON.stringify([mytodo]));
+    }
+})
